458ef1a368a12f344f2d259859d50f8c36dc1768
manoelnetosw <manoelnetosw@gmail.com>
2024-06-10 10:34:35 -0400

    adicionando espaços

--- src/main/java/com/mcmn/bookstore/BookstoreApplication.java
+++ src/main/java/com/mcmn/bookstore/BookstoreApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(BookstoreApplication.class, args);
+		
 	}
 
 }
