6fd63a0fa332b1d10c99a24c09695b680a6b4519
manoelnetosw <manoelnetosw@gmail.com>
2025-02-15 13:56:44 -0500

    criação perfil de dev

--- src/main/java/com/mcmn/bookstore/config/DevConfig.java
+++ src/main/java/com/mcmn/bookstore/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.mcmn.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,17 +9,22 @@ import org.springframework.context.annotation.Profile;
 import com.mcmn.bookstore.services.DBService;
 
 @Configuration
-@Profile("test")
-public class testeConfig {
-	
+@Profile("dev")
+public class DevConfig {
+
 	@Autowired
 	private DBService dbService;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String Strategy;
+
 	@Bean
 	public boolean instanciaBaseDeDados() {
-        System.out.println("⚡ TestConfig: Chamando DBService...");
-        dbService.instanciaBaseDeDados();
-        System.out.println("✅ TestConfig: DBService chamado com sucesso!");
-        return true;
-    }
+		if (Strategy.equals("create")) {
+			System.out.println("⚡ DevConfig: Chamando DBService...");
+			this.dbService.instanciaBaseDeDados();
+			System.out.println("✅ DevConfig: DBService chamado com sucesso!");
+		}
+		return false;
+	}
 }
